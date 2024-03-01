559046f68a06657a71bde5d02b5de49b54e4e13b
GiorgettaR <giorgetta.roberto@gmail.com>
2024-03-01 16:35:58 +0100

    First Commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+// Esercizio 1
+// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+let number = 100; //number
+console.log(number)
+
+// Esercizio 2
+// Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+const pi = 3.14; //number
+console.log(number)
+
+// Esercizio 2/B
+// Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+// pi = 3.1416;
+// verrebbe visualizzato un errore in quanto la variabile pi è di tipo const e non può essere riassegnato
+
+
+// Esercizio 2/C
+// Dichiara una variabile chiamata radius e assegnale il valore 8.
+// Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+// Stampa il valore della variabile circle in console.
+let radius = 8; //number
+let circle = (radius * 2 * pi) //number
+console.log(circle)
+
+// Esercizio 3
+// Dichiara una variabile chiamata name e
+// assegnale il valore “Carlo”.
+// Successivamente, modifica il valore della variabile in “Marco”.
+// Stampa il nuovo valore in console.
+let name = 'Carlo'; //string
+name = 'Marco'; //string
+console.log(name)
